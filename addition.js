d07bd356a570f532a8bd9dadd3a5dc8554b6b7a1
tlcgeyer <tamlincupido2402@gmail.com>
2024-02-07 10:06:10 +0200

    info

---
+++ addition.js
@@ -0,0 +1,16 @@
+// console.log('Addition');
+
+// // Creating a function 
+// function Addition(a,b) {
+//     return a+b 
+// }
+
+// //Allows us to export our function
+// module.exports = {
+//     Addition
+// }
+
+// // Alternative way: If there is one object you need to export, you can make use of this method
+// exports.Addition = (a, b)=> {
+//     return a+ b
+// }
